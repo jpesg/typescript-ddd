2231296877f0be18947d3dcc524657e1dd0e5c1c
javier <jpesgil@gmail.com>
2023-02-11 19:29:07 +0000

    init

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  overrides: [],
+  plugins: ['prettier'],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['tsconfig.json'],
+  },
+  rules: {
+    'no-console': 1,
+    quotes: [
+      'error',
+      'single',
+      { avoidEscape: true, allowTemplateLiterals: true },
+    ],
+    'eol-last': 0,
+    'prettier/prettier': ['error', { singleQuote: true, semi: false }],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+  },
+}
