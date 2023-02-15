0c2332dad1878704100285ffac796702886c4d76
javier <jpesgil@gmail.com>
2023-02-15 17:46:32 +0000

    fix: return type

--- src/app/user/backend/controllers/controller.ts
+++ src/app/user/backend/controllers/controller.ts
@@ -1,6 +1,5 @@
 import type { Request, Response } from 'express'
 
 export interface Controller {
-  // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-  run: <T>(req: Request, res: Response) => Promise<T | void>
+  run: (req: Request, res: Response) => Promise<void>
 }
