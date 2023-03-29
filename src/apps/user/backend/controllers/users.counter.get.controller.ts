15c07cc2e05b2a088fa7e96ece99f159204c3ee7
javier <jpesgil@gmail.com>
2023-03-29 19:49:27 +0100

    feat: users counter controller

---
+++ src/apps/user/backend/controllers/users.counter.get.controller.ts
@@ -0,0 +1,24 @@
+import { type Request, type Response } from 'express'
+
+import httpStatus from 'http-status'
+import { type Controller } from '../../../shared/domain/controllers/controller'
+import { type UsersCounterFinder } from '../../../../contexts/user/users-counter/application/find/users.counter.finder'
+import { UsersCounterNotExist } from '../../../../contexts/user/users-counter/domain/users.counter.not.exist'
+
+export class UsersCounterGetController implements Controller {
+  constructor(private usersCounterFinder: UsersCounterFinder) {}
+  async run(req: Request, res: Response): Promise<void> {
+    try {
+      const count = await this.usersCounterFinder.run()
+
+      res.json({ total: count })
+    } catch (e) {
+      console.log(e)
+      if (e instanceof UsersCounterNotExist) {
+        res.sendStatus(httpStatus.NOT_FOUND)
+      } else {
+        res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR)
+      }
+    }
+  }
+}
